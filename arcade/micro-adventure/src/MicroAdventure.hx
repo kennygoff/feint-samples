abdb70af7ed9109296633e847d3ba3fcac35adf8
Kenny Goff <goff.kenneth@gmail.com>
2021-02-14 17:29:27 -0500

    Setup Micro Adventure

---
+++ arcade/micro-adventure/src/MicroAdventure.hx
@@ -0,0 +1,22 @@
+package;
+
+import feint.Application;
+import feint.assets.macros.ApplicationSettings.ApplicationConfig;
+
+class MicroAdventure extends Application {
+  override public function init() {
+    game.setInitialScene(new scenes.TitleScreenScene());
+  }
+
+  static public function main() {
+    var config:ApplicationConfig = feint.assets.macros.ApplicationSettings.getApplicationConfig();
+    new MicroAdventure({
+      title: config.title,
+      size: {
+        width: config.window.width,
+        height: config.window.height
+      },
+      api: WebGL,
+    });
+  }
+}
---
+++ arcade/micro-adventure/src/scenes/PlayScene.hx
@@ -0,0 +1,9 @@
+package scenes;
+
+import feint.library.BitmapTextRenderSystem;
+import feint.library.BitmapTextComponent;
+import feint.library.PositionComponent;
+import feint.forge.Entity;
+import feint.scene.Scene;
+
+class PlayScene extends Scene {}
